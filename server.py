c65401cb49ffb663be8c6680142a9d5309f618a2
Pedibulle12 <quentin.becherand@gmail.com>
2025-09-26 17:25:19 +0200

    pendu_test

--- server.py
+++ server.py
@@ -1,8 +1,41 @@
-from flask import Flask
-
+from flask import Flask, render_template, request, redirect, url_for
+import random
 app = Flask(__name__)
 
 
-@app.route("/")
+user=""
+mot=""
+
+
+def choix_ligne():
+    """
+    """
+    fichier = open("dictionnaire.txt")
+    lignes = fichier.readlines()
+    nombre_aleatoire=random.randint(0, len(lignes))
+    ligne=lignes[nombre_aleatoire]
+    return ligne
+
+def choix_mot():
+    mot_brut=choix_ligne()
+    mot=mot_brut.split(";")[0]
+    return mot    
+
+
+
+@app.route('/', methods=['GET', 'POST'])
 def home():
-    return "<p>Hello, World!</p>"
+    global user, mot
+    if request.method == 'POST':
+        user=request.form["nom"]
+        return redirect(url_for("jouer"))
+    else:
+        return render_template('home.html')
+    
+@app.route('/play', methods=['GET', 'POST'])
+def jouer():
+    print(choix_mot())
+    return render_template("play.html", nom_joueur=user)
+
+
+
